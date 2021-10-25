34933250e3a776f3e4d580fa14e784589063d16c
Tebalo <btebalo@gmail.com>
2021-10-25 12:05:09 +0200

    Load assets
    The app can now load patterns from a text file.

--- lib/text_Interface/MyCustomForm.dart
+++ lib/text_Interface/MyCustomForm.dart
@@ -1,217 +1,1 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:leamanyi_app/trie/trie.dart';
-import 'package:leamanyi_app/nltoolkit/nltk.dart';
-
-class MyCustomForm extends StatefulWidget{
-  const MyCustomForm({Key key}) :super(key: key);
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
-class _MyCustomFormState extends State<MyCustomForm>{
-  final myController = TextEditingController();
-  Trie trie = new Trie();
-  List<List<String>> tags;
-  Node root;
-  Nltk nltk;
-  _MyCustomFormState(){
-    tags = trie.strip_to_array();
-    root = trie.create_map(tags);
-    nltk = new Nltk();
-  }
-  @override
-  void dispose(){
-    myController.dispose();
-    super.dispose();
-  }
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.home),
-        title: const Text('SETSWANA SA BORRE'),
-        backgroundColor: Colors.black38,
-        foregroundColor: Colors.white54,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.brown,
-            height: MediaQuery.of(context).size.height,
-            child: Body(myController: myController)
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
-          String text = myController.text;
-          //print(text);
-          List tagged = nltk.tag(text);
-          //print(tagged);
-          text = trie.pattern_match(tagged, root);
-          print(text);
-          showDialog(
-              context: context,
-              builder: (context){
-                return AlertDialog(
-                  content: Text("$text"),
-                );
-              }
-          );
-        },
-        tooltip: 'Ntshupegetsa leamanyi!',
-        icon: const Icon(Icons.text_fields),
-        label: Text("Bata leamanyi"),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-}
-
-class Body extends StatelessWidget {
-  const Body({
-    Key key,
-    @required this.myController,
-  }) : super(key: key);
-
-  final TextEditingController myController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      color: Colors.amber,
-      shadowColor: Colors.black12,
-      borderOnForeground: true,
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: <Widget>[
-          TextArea(),
-          ButtonArea(),
-          TextFieldArea(myController: myController),
-        ]
-      ),
-    );
-  }
-}
-
-class TextArea extends StatelessWidget {
-  const TextArea({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 2,
-        borderOnForeground: true,
-        color: Colors.black12,
-
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: RichText(
-            softWrap: true,
-            text: TextSpan(
-              //text: "Leamanyi-Relative #Diphetolelo\n",
-              style: DefaultTextStyle.of(context).style,
-              //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-              children: const <TextSpan>[
-                TextSpan(text: "Leamanyi-Relative #Diphetolelo\n",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                TextSpan(text: "\t\t#Leamnyi: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: "\t\tKe lefoko le le amanyang dilo tse di farologaneng. "
-                    "\t\t\t\t\tLe bopiwa ka legokedi le thito.\n"),
-                TextSpan(text: '\t\t#Sekai : ', style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: 'Monna yo o lemang o itse botshelo.\n'),
-                TextSpan(text: "\t\t\t              /yo o/ => Leamanyi.\n"),
-                TextSpan(text: "\t\t\t              /lemang/ => thito.\n"),
-                TextSpan(text: "Tshidimosetso\n", style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: "\t\t\t1. O ka tsenya senepe se kwadilweng ka mafoko.\n"),
-                TextSpan(text: "\t\t\t     a Setswana.\n"),
-                TextSpan(text: "\t\t\t2. O ka tsaya senepe se kwadilweng ka mafoko a\n"),
-                TextSpan(text: "\t\t\t     ka camera ya Phone.\n"),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonArea extends StatelessWidget {
-  const ButtonArea({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: <Widget>[
-          //Button1(text: "Tsenya Senepe",icon: Icons.image),
-          //Button1(text: "Tsaya Senepe", icon: Icons.add_a_photo),
-          Button1(text: "Tsenya File", icon: Icons.file_upload)
-        ],
-      ),
-    );
-  }
-}
-
-class Button1 extends StatelessWidget {
-  final String text;
-  final IconData icon;
-  const Button1({
-    Key key,
-    @required this.text,
-    @required this.icon,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 7.0),
-      child: ElevatedButton.icon(
-          onPressed: (){
-
-          },
-          label: Text(text),
-          icon: Icon(icon, size: 25,),
-      ),
-    );
-  }
-}
-
-class TextFieldArea extends StatelessWidget {
-  const TextFieldArea({
-    Key key,
-    @required this.myController,
-  }) : super(key: key);
-
-  final TextEditingController myController;
-
-  @override
-  Widget build(BuildContext context) {
-    final maxLines = 100;
-    return Container(
-      margin: const EdgeInsets.only(left: 8.0, right: 8.0),
-      child: TextField(
-        controller: myController,
-        keyboardType: TextInputType.multiline,
-        minLines: 1,
-        maxLines: maxLines,
-        decoration: InputDecoration(
-          hintText: "Tsenya seele sa Setswana.",
-          filled: true,
-          hintStyle: new TextStyle(color: Colors.grey[800]),
-          fillColor: Colors.white70,
-          border: OutlineInputBorder(
-            borderRadius: const BorderRadius.all(
-              const Radius.circular(10.0),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// ignore: unnecessary_importimport 'package:flutter/cupertino.dart';import 'package:flutter/material.dart';import 'package:leamanyi_app/trie/trie.dart';import 'package:leamanyi_app/nltoolkit/nltk.dart';import 'dart:async' show Future;class MyCustomForm extends StatefulWidget{  const MyCustomForm({Key key}) :super(key: key);  @override  _MyCustomFormState createState() => _MyCustomFormState();}class _MyCustomFormState extends State<MyCustomForm>{  final myController = TextEditingController();  Trie trie = new Trie();  List<List<String>> tags;  Node root;  Nltk nltk;  Future<void> getList() async {    setState(() async {      tags = await trie.string_to_array();      root = trie.create_map(tags);      nltk = new Nltk();    });  }  @override  void dispose(){    myController.dispose();    super.dispose();  }  @override  Widget build(BuildContext context){    return Scaffold(      appBar: AppBar(        leading: Icon(Icons.home),        title: const Text('SETSWANA SA BORRE'),        backgroundColor: Colors.black38,        foregroundColor: Colors.white54,      ),      body: SingleChildScrollView(        child: FutureBuilder(          future: getList(),          builder: (context, tags) {            if(tags==null){              return Center(child: CircularProgressIndicator(),);            }else{              return Container(                  color: Colors.brown,                  height: MediaQuery.of(context).size.height,                  child: Body(myController: myController)              );            }          }        ),      ),      floatingActionButton: FloatingActionButton.extended(        onPressed: (){          String text = myController.text;          //print(text);          List tagged = nltk.tag(text);          //print(tagged);          text = trie.pattern_match(tagged, root);          print(text);          showDialog(              context: context,              builder: (context){                return AlertDialog(                  content: Text("$text"),                );              }          );        },        tooltip: 'Ntshupegetsa leamanyi!',        icon: const Icon(Icons.text_fields),        label: Text("Bata leamanyi"),      ),      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,    );  }}class Body extends StatelessWidget {  const Body({    Key key,    @required this.myController,  }) : super(key: key);  final TextEditingController myController;  @override  Widget build(BuildContext context) {    return Card(      elevation: 2,      color: Colors.amber,      shadowColor: Colors.black12,      borderOnForeground: true,      clipBehavior: Clip.antiAlias,      child: Column(        children: <Widget>[          TextArea(),          ButtonArea(),          TextFieldArea(myController: myController),        ]      ),    );  }}class TextArea extends StatelessWidget {  const TextArea({    Key key,  }) : super(key: key);  @override  Widget build(BuildContext context) {    return Padding(      padding: const EdgeInsets.all(8.0),      child: Card(        elevation: 2,        borderOnForeground: true,        color: Colors.black12,        child: Padding(          padding: const EdgeInsets.all(8.0),          child: RichText(            softWrap: true,            text: TextSpan(              //text: "Leamanyi-Relative #Diphetolelo\n",              style: DefaultTextStyle.of(context).style,              //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),              children: const <TextSpan>[                TextSpan(text: "Leamanyi-Relative #Diphetolelo\n",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),                TextSpan(text: "\t\t#Leamnyi: ", style: TextStyle(fontWeight: FontWeight.bold)),                TextSpan(text: "\t\tKe lefoko le le amanyang dilo tse di farologaneng. "                    "\t\t\t\t\tLe bopiwa ka legokedi le thito.\n"),                TextSpan(text: '\t\t#Sekai : ', style: TextStyle(fontWeight: FontWeight.bold)),                TextSpan(text: 'Monna yo o lemang o itse botshelo.\n'),                TextSpan(text: "\t\t\t              /yo o/ => Leamanyi.\n"),                TextSpan(text: "\t\t\t              /lemang/ => thito.\n"),                TextSpan(text: "Tshidimosetso\n", style: TextStyle(fontWeight: FontWeight.bold)),                TextSpan(text: "\t\t\t1. O ka tsenya senepe se kwadilweng ka mafoko.\n"),                TextSpan(text: "\t\t\t     a Setswana.\n"),                TextSpan(text: "\t\t\t2. O ka tsaya senepe se kwadilweng ka mafoko a\n"),                TextSpan(text: "\t\t\t     ka camera ya Phone.\n"),              ],            ),          ),        ),      ),    );  }}class ButtonArea extends StatelessWidget {  const ButtonArea({    Key key,  }) : super(key: key);  @override  Widget build(BuildContext context) {    return Padding(      padding: const EdgeInsets.all(8.0),      child: Row(        children: <Widget>[          //Button1(text: "Tsenya Senepe",icon: Icons.image),          //Button1(text: "Tsaya Senepe", icon: Icons.add_a_photo),          Button1(text: "Tsenya File", icon: Icons.file_upload)        ],      ),    );  }}class Button1 extends StatelessWidget {  final String text;  final IconData icon;  const Button1({    Key key,    @required this.text,    @required this.icon,  }) : super(key: key);  @override  Widget build(BuildContext context) {    return Padding(      padding: const EdgeInsets.only(left: 7.0),      child: ElevatedButton.icon(          onPressed: (){          },          label: Text(text),          icon: Icon(icon, size: 25,),      ),    );  }}class TextFieldArea extends StatelessWidget {  const TextFieldArea({    Key key,    @required this.myController,  }) : super(key: key);  final TextEditingController myController;  @override  Widget build(BuildContext context) {    final maxLines = 100;    return Container(      margin: const EdgeInsets.only(left: 8.0, right: 8.0),      child: TextField(        controller: myController,        keyboardType: TextInputType.multiline,        minLines: 1,        maxLines: maxLines,        decoration: InputDecoration(          hintText: "Tsenya seele sa Setswana.",          filled: true,          hintStyle: new TextStyle(color: Colors.grey[800]),          fillColor: Colors.white70,          border: OutlineInputBorder(            borderRadius: const BorderRadius.all(              const Radius.circular(10.0),            ),          ),        ),      ),    );  }}
--- lib/trie/trie.dart
+++ lib/trie/trie.dart
@@ -1,4 +1,7 @@
 import 'dart:async' show Future;
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:leamanyi_app/nltoolkit/nltk.dart';
 
@@ -30,60 +33,77 @@ class Trie{
       }
     }
   }
-  Node create_map(List tags){
+  Node create_map(List<List<String>> tags){
     Node node = new Node();
-    for(List tag in tags){
+    print(tags);
+    for(List<String> tag in tags){
       //print(tag[0]);
       //print(tag.sublist(1));
       create(tag[0], tag.sublist(1), node);
     }
     return node;
   }
-  Future<List<String>> string_to_array() async{
-    String posTags = await rootBundle.loadString('assets/patterns.txt');
-    //String texts = nl.sent_tokenize(pos_tags) as String;
-    List texts = posTags.split("\n");
-    List tags = [];
-    for(String sent in texts){
-      sent = sent.replaceAll(">}","").replaceAll("><","").replaceAll("{<","").replaceAll("\r"," ");
-      List sents = sent.split(" ");
-      //print(sents);
-      tags.addAll(sents);
-    }
-    return tags;
+  Future<List<List<dynamic>>> string_to_array() async{
+     String posTags = await rootBundle.loadString('assets/patterns.txt');
+     posTags = posTags.replaceAll("{<", "").replaceAll("><", " ");
+     List lines = posTags.split(">}");
+
+     var patterns = [];
+     for(var line in lines){
+       patterns.add(line.split(" "));
+     }
+
+    return patterns;
+  }
+  handleLines(List<String> lines){
+    for(var I in lines) log('$lines');
   }
   List<List<String>> strip_to_array(){
-    String posTags = """{<CC7><CC7><VRB>}
-    {<CC7><CC7><L01><VRB>}
+    //String test = await rootBundle.loadString('assets/patterns.txt');
+    //print(test);
+    var config = new File("assets/patterns.txt");
+    config.readAsLines().then(handleLines);
+    log('$config');
+    String posTags = """{<CC7><CC7><VRB+ng>}
+    {<CC3><CC3><VRB+ng>}
+    {<C15><C15><VRB+ng>}
+    {<C12><C13><VRB+ng>}
+    {<C12><C13><VRB+ng>}
+    {<CC9><C10><VRB+ng>}
+    {<CC1><CC4><VRB+ng>}
+    {<CC7><CC7><L01><VRB+ng>}
     {<CC7><CC7><ADJ8>}
-    {<CC7><CC7><L02><VRB>}
-    {<CC7><CC7><L03><VRB>}
-    {<CC7><CC7><L04><CC7><VRB>}
-    {<CC7><CC7><L02><L05><CC7><VRB>}
-    {<CC7><CC7><L03><L06><CC7><VRB>}
-    {<CC7><CC7><L03><L06><CC7><L07><CC7><VRB>}
-    {<CC7><CC7><CC8><L08><CC7><VRB>}
-    {<CC7><CC7><L02><L01><L10><CC7><VRB>}
-    {<CC7><CC7><L02><L11><C15><VRB>}
-    {<CC5><CC5><L01><CC8><L01><L10><L12><VRB>}
-    {<CC7><CC7><L02><C11><VRB>}
-    {<CC7><CC7><CC8><L01><L10><CC7><C11><VRB>}
-    {<CC7><CC7><L02><L05><CC7><L02><VRB>}
-    {<CC7><CC7><L02><L05><CC7><L03><VRB>}
-    {<CC7><CC8><L01><L10><CC7><L03><CC7><VRB>}
-    {<CC7><CC7><CC8><L01><L10><CC7><L03><C15><VRB>}
-    {<CC7><CC7><L02><L13><CC7><VRB>}""";
+    {<CC7><CC7><L02><VRB+ng>}
+    {<CC7><CC7><L03><VRB+ng>}
+    {<CC7><CC7><L04><CC7><VRB+ng>}
+    {<CC7><CC7><L02><L05><CC7><VRB+ng>}
+    {<CC7><CC7><L03><L06><CC7><VRB+ng>}
+    {<CC7><CC7><L03><L06><CC7><L07><CC7><VRB+ng>}
+    {<CC7><CC7><CC8><L08><CC7><VRB+ng>}
+    {<CC7><CC7><L02><L01><L10><CC7><VRB+ng>}
+    {<CC7><CC7><L02><L11><C15><VRB+ng>}
+    {<CC5><CC5><L01><CC8><L01><L10><L12><VRB+ng>}
+    {<CC7><CC7><L02><C11><VRB+ng>}
+    {<CC7><CC7><CC8><L01><L10><CC7><C11><VRB+ng>}
+    {<CC7><CC7><L02><L05><CC7><L02><VRB+ng>}
+    {<CC7><CC7><L02><L05><CC7><L03><VRB+ng>}
+    {<CC7><CC8><L01><L10><CC7><L03><CC7><VRB+ng>}
+    {<CC7><CC7><CC8><L01><L10><CC7><L03><C15><VRB+ng>}
+    {<CC7><CC7><L02><L13><CC7><VRB+ng>}""";
+
     //String texts = nl.sent_tokenize(pos_tags) as String;
-    List texts = posTags.split("\n");
+    //List texts = posTags.split("\n");
     List<List<String>> tags = [["CC7","CC7","VRB+ng"],["CC3","CC3","VRB+ng"],["C15","C15","VRB+ng"],["C12","C13","VRB+ng"],["C11","C11","VRB+ng"],["CC4","CC4","VRB+ng"],["CC8","CC8","ADJ8"],["CC9","C10","VRB+ng"],["CC6","CC6","VRB+ng"],["CC1","CC4","VRB+ng"]];
     //int i = 0;
-    //for(String sent in texts){
-      //sent = sent.replaceAll("{<","").replaceAll(">}","").replaceAll("><"," ").replaceAll("\r"," ");
-      //List<String> sents = sent.split(" ");
+    //List<List<String>> tagset = [];
+   // for(String sent in texts){
+     // sent = sent.replaceAll("{<","").replaceAll(">}","").replaceAll("><"," ").replaceAll("\r"," ");
+     // print(sent);
+     // List<String> sents = sent.split(" ");
       //print(sents);
-      //tags[i].addAll(sents);
-      //i++;
+      //tagset.add(sents);
     //}
+    //print(tagset);
     return tags;
   }
   void futureToList(List lst) async{
@@ -93,7 +113,7 @@ class Trie{
     String tagg = "No leamanyi";
     if(node.children.containsKey(tup.item2)){
       node = node.children[tup.item2];
-      identified+=tup.item1+" ";
+      identified+=tup.item2+" ";
 
       if(arr.length >=1){
         tagg = search(arr[0], arr.sublist(1), node, identified);
@@ -106,13 +126,12 @@ class Trie{
         tagg = "("+identified.trimRight()+")/Leamanyi";
       }
     }
-    print(tagg);
     return tagg;
   }
   String pattern_match(List tagged, Node node){
-    String tag_lea = "";
+    String tagLea = "";
     for(var tags in tagged) {
-      List a_mod = [];
+      List aMod = [];
       //for(int i = 0;i<tags.length;i++){
       //if(node.children.containsValue(tags[1])){
       // a_mod = tags.sublist(1,tags.length-1);
@@ -120,13 +139,13 @@ class Trie{
       //}
       if(node.children.containsKey(tags.item2)){
         //print(node.children[tags.item2]);
-        a_mod = tagged.sublist(1,tagged.length);
-        tag_lea = search_trie(a_mod, node);
+        aMod = tagged.sublist(1,tagged.length);
+        tagLea = search_trie(aMod, node);
       }
     //}
     }
     //print(tag_lea);
-    return tag_lea;
+    return tagLea;
   }
 
   String search_trie(List probale,Node node){
